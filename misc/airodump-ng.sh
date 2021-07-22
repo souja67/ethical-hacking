6c2ab1de3c42f845f1534fac3d78d053dbb350b6
souja67 <souja67@gmail.com>
2021-07-21 20:32:16 -0400

    Create airodump-ng.sh

---
+++ misc/airodump-ng.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+ifconfig | grep wlan0 && ifconfig wlan0 down
+ifconfig wlan0 up
+airmon-ng start wlan0
+ifconfig | grep wlan0 && ifconfig wlan0 down && macchanger -r wlan0
+ifconfig wlan0 up
+airodump-ng wlan0

dc8ed62d161139adb2d542cb8a522c77df2ab4df
souja67 <souja67@users.noreply.github.com>
2021-07-21 22:28:27 -0400

    added delays to scripts
    added delays in scripts to prevent wireless card lockup issue

--- misc/airodump-ng.sh
+++ misc/airodump-ng.sh
@@ -1,9 +1,17 @@
-#!/bin/bash -x
+#!/bin/bash -v
 
 ifconfig | grep wlan0 && ifconfig wlan0 down
+sleep 1
 ifconfig wlan0 up
+sleep 1
 airmon-ng check kill
+sleep 1
 airmon-ng start wlan0
-ifconfig | grep wlan0 && ifconfig wlan0 down && macchanger -r wlan0
+sleep 1
+ifconfig wlan0 down
+sleep 1
+macchanger -r wlan0
+sleep 1
 ifconfig wlan0 up
+sleep 1
 airodump-ng wlan0
--- misc/rtl8187.sh
+++ misc/rtl8187.sh
@@ -2,10 +2,15 @@
 
 echo Remove RTL8187 Module
 rmmod rtl8187
+sleep 1
 rfkill block all
+sleep 1
 rfkill unblock all
+sleep 1
 echo Add RTL8187 Module
 modprobe rtl8187
+sleep 1
 rfkill unblock all
+sleep 1
 echo Bring wlan0 Up
 ifconfig wlan0 up
